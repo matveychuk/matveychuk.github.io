28677088fcebe2e204fbefbda5225c3ab9700a3c
matveychuk <matveychuk.a@gmail.com>
2020-03-03 11:37:40 +0200

    Updates

---
+++ precache-manifest.6f8dd37159c8afc973448225a986349c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a78d0a167e583c61f30b909a9172cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37c2d50558b3ab08aedc",
+    "url": "/static/css/main.f16ce786.chunk.css"
+  },
+  {
+    "revision": "fef5569263ecd557666b",
+    "url": "/static/js/2.1a982024.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.1a982024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c2d50558b3ab08aedc",
+    "url": "/static/js/main.432e9f24.chunk.js"
+  },
+  {
+    "revision": "590829850b8b8a93d37b",
+    "url": "/static/js/runtime-main.33a978d5.js"
+  },
+  {
+    "revision": "56e14f2d62d3bb0835847c34781b0c99",
+    "url": "/static/media/analytics.56e14f2d.svg"
+  },
+  {
+    "revision": "50151ca39e5c6ae6659200ec333674da",
+    "url": "/static/media/arrow.50151ca3.svg"
+  },
+  {
+    "revision": "c78a4447759297bca936bc0f81d3f6a7",
+    "url": "/static/media/avatar.c78a4447.jpg"
+  },
+  {
+    "revision": "d705b0aef3d6b6ae7c45ae4c6e4a6f90",
+    "url": "/static/media/babyshop.d705b0ae.png"
+  },
+  {
+    "revision": "079cb576ecabc98a42bd8968620c6717",
+    "url": "/static/media/bg_bio_ornament.079cb576.jpg"
+  },
+  {
+    "revision": "682c6082c3603e8e23f2df3c0579ea26",
+    "url": "/static/media/bg_footer.682c6082.jpg"
+  },
+  {
+    "revision": "7a8efb01aa0e8e6320f10631f15c5a7f",
+    "url": "/static/media/bg_header.7a8efb01.png"
+  },
+  {
+    "revision": "a39195b8278b2b2cc0f6ad03ddc2bd4f",
+    "url": "/static/media/clothes.a39195b8.png"
+  },
+  {
+    "revision": "1a5f883ae8f7e10c2ad9a9dab850aaaf",
+    "url": "/static/media/clothes_active.1a5f883a.png"
+  },
+  {
+    "revision": "0526f2c3bad8826a5e84cbc9c19cf2be",
+    "url": "/static/media/email.0526f2c3.svg"
+  },
+  {
+    "revision": "1a070838f2503c69c0481f26a81479a7",
+    "url": "/static/media/finance.1a070838.png"
+  },
+  {
+    "revision": "1f27b06598cd67da3a283788197dc481",
+    "url": "/static/media/finance_active.1f27b065.png"
+  },
+  {
+    "revision": "7e3ff9de2ff0a5bfb1a5e5f1acbcdfcc",
+    "url": "/static/media/galereya.7e3ff9de.png"
+  },
+  {
+    "revision": "01a5600eb0cfa6d3a7d0a52eaba97a8a",
+    "url": "/static/media/gametree.01a5600e.svg"
+  },
+  {
+    "revision": "a5ad5cbef23a48fe458e1eface65efa5",
+    "url": "/static/media/gametree.a5ad5cbe.png"
+  },
+  {
+    "revision": "62f7acead848353830a96223d7c7f663",
+    "url": "/static/media/gametree_active.62f7acea.png"
+  },
+  {
+    "revision": "50bdf123cecbcc286762ff40144e7d04",
+    "url": "/static/media/ifrs.50bdf123.svg"
+  },
+  {
+    "revision": "ec593f02ad8002272d5d641a5a6036dd",
+    "url": "/static/media/logo.ec593f02.svg"
+  },
+  {
+    "revision": "5f4454c5a488ca96584f2413daaf1c76",
+    "url": "/static/media/memory.5f4454c5.png"
+  },
+  {
+    "revision": "c2e425b39d0c35b3d8296c31f0bbe32f",
+    "url": "/static/media/memory.c2e425b3.gif"
+  },
+  {
+    "revision": "bbc8e91f0b32bfb83913eec29cbd183a",
+    "url": "/static/media/memory_active.bbc8e91f.png"
+  },
+  {
+    "revision": "550ea1d8b48c40482a53e34b67064e8a",
+    "url": "/static/media/name_image.550ea1d8.png"
+  },
+  {
+    "revision": "40dc4316d4f7b07966c8d4f63cf07ab6",
+    "url": "/static/media/phone.40dc4316.svg"
+  },
+  {
+    "revision": "9ff2dd505a91fd6369c467d716ff7dc5",
+    "url": "/static/media/poker.9ff2dd50.png"
+  },
+  {
+    "revision": "e5391e087b64ada3a715dac383137c75",
+    "url": "/static/media/poker.e5391e08.gif"
+  },
+  {
+    "revision": "d5872ff5593885a5198c66e744421fca",
+    "url": "/static/media/poker_active.d5872ff5.png"
+  },
+  {
+    "revision": "f172f88c80dc78a057b28e544c7d6397",
+    "url": "/static/media/prokat.f172f88c.png"
+  },
+  {
+    "revision": "4522a518a779d3ed632fd67d8c7e1c7f",
+    "url": "/static/media/prokat_active.4522a518.png"
+  },
+  {
+    "revision": "e80d00c86279da175c0cef0443b680d8",
+    "url": "/static/media/skype.e80d00c8.svg"
+  }
+]);
--- src/components/common/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from "./Header";
-export { default as Footer } from "./Footer";
-export { default as BurgerMenu } from "./BurgerMenu";
-export { default as Navbar } from "./Navbar";
-export { default as Testimonial } from "./Testimonial";
